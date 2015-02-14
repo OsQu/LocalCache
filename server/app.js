3d703cb12655d2f384ffe2914d6db742fd47c8d6
Oskari Virtanen <oskari@flowdock.com>
2015-02-14 14:18:47 +0200

    Add some semicolons

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var logger = require("winston");
 var app = express();
 
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 8080;
 
 app.get("/", function(req, res) {
   res.send("It's alive!");
@@ -10,5 +10,5 @@ app.get("/", function(req, res) {
 
 var server = app.listen(PORT, function() {
   var address = server.address();
-  logger.info("Started server: %s:%s", address.address, address.port)
+  logger.info("Started server: %s:%s", address.address, address.port);
 });
